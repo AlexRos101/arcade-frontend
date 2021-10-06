d29887085451d3ddb526c8cd5f1dd380ad78cd19
Alex <ros1996101@gmail.com>
2021-10-06 21:15:40 +0800

    - responsive updated

--- src/views/Market/components/CardSlider.tsx
+++ src/views/Market/components/CardSlider.tsx
@@ -21,9 +21,46 @@ interface Props {
 
 
 const CardSlider: React.FC<Props>= (props) => {
+    const settings = {
+        slidesToShow: 5,
+        swipeToSlide: true,
+        draggable: true,
+        arrows: false,
+        rows: props.rows,
+        responsive: [
+            {
+                breakpoint: 1080,
+                settings: {
+                    slidesToShow: 5,
+                    variableWidth: true,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 4,
+                    variableWidth: true,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 3,
+                    variableWidth: true,
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    variableWidth: true,
+                }
+            }
+        ]
+    }
     return (
-        <div className="market-slide">
-            <Slider slidesToShow={5} swipeToSlide={true} draggable={true} arrows={false} rows={props.rows}> 
+        <div className={`market-slide-${props.rows}`}>
+            <Slider {...settings}> 
             {
                 props.context.map((map, index) => {
                     return (
