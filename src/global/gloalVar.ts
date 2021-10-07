73c7d6db1d40b7679abb47a74bb4b2c6cb1aca10
krozy <krozy0820@hotmail.com>
2021-10-08 05:38:17 +0800

    - lint processed

--- src/global/gloalVar.ts
+++
@@ -1,7 +0,0 @@
-import { store } from 'state-pool'
-
-export const InitializeGlobalVar = (): void => {
-  store.setState('account', '')
-  store.setState('commentState', 0)
-  store.setState('openDiscussionRule', false)
-}
